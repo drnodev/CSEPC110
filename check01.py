099f076e122fea06ff9040a00506311625de2c92
drnodev <alexanortega@gmail.com>
2021-05-16 00:29:44 -0600

    From Here!

---
+++ check01.py
@@ -0,0 +1,9 @@
+""" 
+File:check01.py
+Author: NO
+
+Purspose:  This is the first program in python, i am so exited, this program display text on terminal
+"""
+
+print('Hello World!')
+
---
+++ prove01_colors.py
@@ -0,0 +1,12 @@
+""" 
+File: prove01_colors.py
+Author: NO
+
+Purpuse: This program ask your favorite color, and give you back
+"""
+
+color = input("Please type your favorite color: ")
+print("Your favorite color is: {} Woow!!!!! That color is gorgeous".format(color))
+dislike = input("What color do you dislike? ")
+print(":( .... Oh!!!! No!!! {} is a nice color.".format(dislike))
+print("Bye :P")
